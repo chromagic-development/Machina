ea214b61389aa7b20c2358dd7cedfe2e375e0bd4
Bruce Alexander <chromagic@gmail.com>
2024-06-09 09:23:57 -0400

    Added GetStockQuotePlugin and GetHeadlinesPlugin

---
+++ GetHeadlinesPlugin/GetHeadlines/GetHeadlines.cs
@@ -0,0 +1,113 @@
+// GetHeadlines VM plugin: Get top three national news headlines_p
+// Bruce Alexander 2024 v1
+
+using vmAPI;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GetHeadlinesPluginCS
+{
+    public static class Interface_Manager
+    {
+        public static VoiceMacro vmInstance = new VoiceMacro();
+
+        //The rest of interface code here.
+
+    }
+
+    public class VoiceMacro : vmInterface
+    {
+        public string DisplayName => "GetHeadlines";
+
+        public string Description => "Get top three national news headlines_p\r\nArgument 1: News API key";
+
+        public string ID => "6e85fa50-8496-4d7c-a289-fb53e82e7ff6";
+
+        public void Init()
+        {
+            // Initialization routines go here
+        }
+
+        public void ReceiveParams(string Param1, string Param2, string Param3, bool Synchron)
+        {
+            // Remove quotes from arguments if present
+            Param1 = Param1.Replace("\"", "");
+
+            Task.Run(async () =>
+            {
+                string response = await GetHeadlines(Param1);
+
+                // Set headlines_p string with ChatGPT response
+                vmCommand.SetVariable("headlines_p", response);
+
+                // Show command in VoiceMacro's log in blue as information
+                vmCommand.AddLogEntry(response, Color.Blue, ID, "H", "Headlines response received");
+            });
+        }
+
+        public void ProfileSwitched(string ProfileGUID, string ProfileName)
+        {
+            // GetCommand plugin received profile switching to profile
+        }
+
+        public void Dispose()
+        {
+            // Stop activities because VoiceMacro is shutting down
+        }
+
+        private const string UserAgent = "GetHeadlines/1.0 (newsmanupdate2024@gmail.com)";
+
+        // Get top three national news headlines_p
+        // Argument 1: News API key
+        private static async Task<string> GetHeadlines(string apiKey)
+        {
+            string apiUrl = $"https://newsapi.org/v2/top-headlines?country=us&apiKey={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseObject = JsonConvert.DeserializeObject<NewsApiResponse>(responseString);
+                    if (responseObject?.articles?.Count >= 3)
+                    {
+                        string headline1 = responseObject.articles[0]?.title ?? "No title";
+                        string headline2 = responseObject.articles[1]?.title ?? "No title";
+                        string headline3 = responseObject.articles[2]?.title ?? "No title";
+
+                        return $"The top three national news stories from the previous day are {headline1} and ... {headline2} and ... {headline3}."
+                        .Replace("\"", "")
+                        .Replace("'", "")
+                        .Replace(",", " ... ");
+                    }
+                    else
+                    {
+                        return "Not enough news articles found.";
+                    }
+                }
+                else
+                {
+                    return responseString;
+                }
+            }
+        }
+
+        public class NewsApiResponse
+        {
+            public List<Article> articles { get; set; }
+        }
+
+        public class Article
+        {
+            public string title { get; set; }
+        }
+    }
+}
---
+++ GetStockQuotePlugin/GetStockQuote/GetStockQuote.cs
@@ -0,0 +1,124 @@
+// GetStockQuote VM plugin: Get stock price_p from symbol
+// Bruce Alexander 2024 v1
+
+using vmAPI;
+using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetStockQuotePluginCS
+{
+    public static class Interface_Manager
+    {
+        public static VoiceMacro vmInstance = new VoiceMacro();
+
+        //The rest of interface code here.
+
+    }
+
+    public class VoiceMacro : vmInterface
+    {
+        public string DisplayName => "GetStockQuote";
+
+        public string Description => "Get stock price_p from symbol\r\nArgument 1: Alpha Vantage API key\r\nArgument 2: Stock symbol";
+
+        public string ID => "c079bdce-5441-4f98-bfc2-ab992cb7256e";
+
+        public void Init()
+        {
+            // Initialization routines go here
+        }
+
+        public void ReceiveParams(string Param1, string Param2, string Param3, bool Synchron)
+        {
+            // Remove quotes from arguments if present
+            Param1 = Param1.Replace("\"", "");
+            Param2 = Param2.Replace("\"", "");
+
+            Task.Run(async () =>
+            {
+                string response = await GetQuote(Param1, Param2);
+
+                // Set price_p string with Alpha Vantage response
+                vmCommand.SetVariable("price_p", response);
+
+                // Show command in VoiceMacro's log in blue as information
+                vmCommand.AddLogEntry(response, Color.Blue, ID, "Q", "Stock quote received");
+            });
+        }
+
+        public void ProfileSwitched(string ProfileGUID, string ProfileName)
+        {
+            // GetCommand plugin received profile switching to profile
+        }
+
+        public void Dispose()
+        {
+            // Stop activities because VoiceMacro is shutting down
+        }
+
+        // Get stock price_p from symbol
+        // Argument 1: Alpha Vantage API key
+        // Argument 2: Stock symbol
+        private static async Task<string> GetQuote(string apiKey, string stockSymbol)
+        {
+            string apiUrl = $"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stockSymbol}&apikey={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                string responseString = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseObject = JsonConvert.DeserializeObject<AlphaVantageResponse>(responseString);
+
+                    if (responseObject?.GlobalQuote != null)
+                    {
+                        string symbol = responseObject.GlobalQuote.Symbol;
+                        string price = RoundUpToTwoDecimalPlaces(responseObject.GlobalQuote.Price);
+                        string changePercent = RoundUpToTwoDecimalPlaces(responseObject.GlobalQuote.ChangePercent.TrimEnd('%'));
+
+                        return $"{symbol} shares are currently trading at {price} dollars with a change of {changePercent}% from the previous close.";
+                    }
+                }
+                else
+                {
+                    return "Error retrieving stock quote from Alpha Vantage API.";
+                }
+
+                return null;
+            }
+        }
+
+        private static string RoundUpToTwoDecimalPlaces(string value)
+        {
+            if (decimal.TryParse(value, out decimal decimalValue))
+            {
+                decimal roundedValue = Math.Ceiling(decimalValue * 100) / 100;
+                return roundedValue.ToString("F2");
+            }
+            return value;
+        }
+		
+        public class AlphaVantageResponse
+        {
+            [JsonProperty("Global Quote")]
+            public GlobalQuote GlobalQuote { get; set; }
+        }
+
+        public class GlobalQuote
+        {
+            [JsonProperty("01. symbol")]
+            public string Symbol { get; set; }
+
+            [JsonProperty("05. price")]
+            public string Price { get; set; }
+
+            [JsonProperty("10. change percent")]
+            public string ChangePercent { get; set; }
+        }
+    }
+}

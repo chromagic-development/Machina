d450aadda102b5fffb98a664a1199afc657da12a
Bruce Alexander <chromagic@gmail.com>
2024-07-22 11:42:37 -0400

    Replaced Picovoice Porcupine with custom wake word engine and branding

--- WakeWordEngine/Machina/obj/Release/net6.0/Machina.AssemblyInfo.cs
+++ WakeWordEngine/Machina/obj/Release/net6.0/Machina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chromagic Development")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Bruce Alexander 2024")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024 Bruce Alexander")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Machina")]

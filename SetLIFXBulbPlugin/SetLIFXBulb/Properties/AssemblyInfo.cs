9481652b3f66f408ab4ae102cc1e1cbecdbb2d04
Bruce Alexander <chromagic@gmail.com>
2025-04-27 16:36:50 -0400

    Added SetLIFXBulb VM plugin to set LIFX light bulbs on\off

---
+++ SetLIFXBulbPlugin/SetLIFXBulb/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("SetLIFXBulb for VoiceMacro")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Chromagic Development")]
+[assembly: AssemblyProduct("GetCommand")]
+[assembly: AssemblyCopyright("Copyright © 2025 Bruce Alexander")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("02d7d1a5-53a6-40d6-bddf-892cf9e66faf")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SetLIFXBulbPlugin/SetLIFXBulb/SetLIFXBulb.cs
@@ -0,0 +1,96 @@
+// SetLIFXBulb VM plugin: Set LIFX light bulb on or off
+// v1.0.0.0
+// Copyright © 2025 Bruce Alexander
+// vmAPI Library Copyright © 2018-2019 FSC-SOFT
+// This software is licensed under the MIT License. See LICENSE file for details.
+
+using vmAPI;
+using System;
+using System.Drawing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SetLIFXBulbPlugin
+{
+    public static class Interface_Manager
+    {
+        public static VoiceMacro vmInstance = new VoiceMacro();
+    }
+
+    public class VoiceMacro : vmInterface
+    {
+        public string DisplayName => "SetLIFXBulb";
+
+        public string Description => "Set LIFX light bulb on or off\r\nArgument 1: LIFX Access Token\r\nArgument 2: Bulb Label\r\nArgument 3: Power (on/off)";
+
+        public string ID => "906a41d9-68e2-4394-9272-b6293a2eb2f1";
+
+        public void Init()
+        {
+            // Initialization routines
+        }
+
+        public void ReceiveParams(string Param1, string Param2, string Param3, bool Synchron)
+        {
+            // Remove quotes from arguments if present
+            Param1 = Param1.Replace("\"", "");
+            Param2 = Param2.Replace("\"", "");
+            Param3 = Param3.Replace("\"", "");
+
+            Task.Run(async () =>
+            {
+                string response = await SetLIFXState(Param1, Param2, Param3);
+
+                // Set response string to VM variable
+                vmCommand.SetVariable("lifx_response", response);
+
+                // Log in blue if success, red if error
+                Color logColor = response.StartsWith("Power") ? Color.Blue : Color.Red;
+                vmCommand.AddLogEntry(response, logColor, ID, "L", "LIFX bulb power set");
+            });
+        }
+
+        public void ProfileSwitched(string ProfileGUID, string ProfileName)
+        {
+            // Profile switching handler
+        }
+
+        public void Dispose()
+        {
+            // Cleanup when VoiceMacro shuts down
+        }
+
+        private static async Task<string> SetLIFXState(string accessToken, string label, string powerState)
+        {
+            string safeLabel = Uri.EscapeDataString(label);
+            string apiUrl = $"https://api.lifx.com/v1/lights/label:{safeLabel}/state";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+
+                var payload = new
+                {
+                    power = powerState.ToLower(),   // "on" or "off"
+                };
+
+                string jsonPayload = JsonConvert.SerializeObject(payload);
+                HttpContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PutAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return $"Power {powerState} {label}";
+                }
+                else
+                {
+                    string errorDetails = await response.Content.ReadAsStringAsync();
+                    return $"Error: {response.StatusCode} - {errorDetails}";
+                }
+            }
+        }
+    }
+}
